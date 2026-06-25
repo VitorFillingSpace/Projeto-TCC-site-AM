da0f316c6f830d06db2b3d8515974e6dc2d9865a
VitorFillingSpace <vpinho845@gmail.com>
2026-06-25 09:48:20 -0300

    teclado calculadora

--- calculadora.js
+++ calculadora.js
@@ -19,3 +19,23 @@ function calcular() {
         display.value = "Erro";
     }
 }
+
+document.addEventListener("keydown", function (evento) {
+    const tecla = evento.key
+    if("0123456789+-*/.".includes (tecla)){
+    adicionar(tecla)
+    }
+
+    if(tecla === "Enter" || tecla === "="){
+        calcular();
+    }
+
+    if (tecla === "Escape"){
+        limpar();
+    }
+
+    if (tecla === "Backspace"){
+        apagar();
+    }
+
+})
